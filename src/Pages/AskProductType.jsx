e18ebaf674ecdc29242078cab5868d6ed2ad5123
Muhammadhamzamunir <hamzamunir7171@gmail.com>
2024-06-11 22:12:07 +0500

    ok

--- src/Pages/AskProductType.jsx
+++ src/Pages/AskProductType.jsx
@@ -6,16 +6,16 @@ export default function AskProductType() {
 
     return (
         <div className="flex justify-center items-center mt-20">
-            <div className="flex gap-7 items-center justify-center">
+            <div className="flex gap-7  flex-col md:flex-row items-center justify-center">
                 <Link to="/bakery/add-product-cake">
-                    <div className="border-2 border-pink-500 rounded-lg p-4 cursor-pointer" >
-                        <img src="/myLogo.png" alt="Cake" className="w-24 h-24 mb-2" />
-                        <h1 className="text-pink-500">Is It Cake?</h1>
+                    <div className="border-2 border-pink-300 rounded-lg p-4 cursor-pointer" >
+                        <img src="/cake.png" alt="Cake" className="w-44 h-44 mb-2" />
+                        <h1 className="text-pink-500 text-center">Is It Cake?</h1>
                     </div></Link>
                 <Link to="/bakery/add-product-party-item">
-                <div className="border-2 border-pink-500 rounded-lg p-4 cursor-pointer " >
-                    <img src="/myLogo.png" alt="Party Item" className="w-24 h-24 mb-2" />
-                    <h1 className="text-pink-500">Is It Party Item?</h1>
+                <div className="border-2 border-pink-300 rounded-lg p-4 cursor-pointer " >
+                    <img src="/partyItem.png" alt="Party Item" className="w-44 h-44 mb-2" />
+                    <h1 className="text-pink-500 text-center">Is It Party Item?</h1>
                 </div>
                 </Link>
             </div>
